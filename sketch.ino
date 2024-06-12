50e3567fdbda8fa5a8af6a4605b1d46784c51f22
Miguel Gonzales <88881293+xmrsv@users.noreply.github.com>
2024-06-11 20:29:56 -0500

    Formateo

--- sketch.ino
+++ sketch.ino
@@ -10,90 +10,84 @@ const int aumenta = 100;
 int leds[5] = {8, 9, 10, 11, 12};
  
 void setup() {
-  // Inicializamos los pines de los LEDs como salida y a estado bajo
-  for (int i = 0; i < 5; i++)
-  {
-    pinMode(leds[i], OUTPUT);
-    digitalWrite(leds[i], LOW);
-  }
+    // Inicializamos los pines de los LEDs como salida y a estado bajo
+    for (int i = 0; i < 5; i++)
+    {
+        pinMode(leds[i], OUTPUT);
+        digitalWrite(leds[i], LOW);
+    }
+    // Asignamos la velocidad mínima
+    velocidad = minima;
  
-  // Asignamos la velocidad mínima
-  velocidad = minima;
- 
-  // Configuramos los pines de interrupciones para que
-  // detecten un cambio de bajo a alto
-  attachInterrupt(digitalPinToInterrupt(2), velocidadMenos, RISING);
-  attachInterrupt(digitalPinToInterrupt(3), velocidadMas, RISING);
- 
+    // Configuramos los pines de interrupciones para que
+    // detecten un cambio de bajo a alto
+    attachInterrupt(digitalPinToInterrupt(2), velocidadMenos, RISING);
+    attachInterrupt(digitalPinToInterrupt(3), velocidadMas, RISING);
 }
  
 void loop() {
-  // Este primer bucle recorre el array de izquierda a derecha
-  for (int i = 0; i < 5; i++)
-  {
-    // Solo para el segundo pin y consecutivos apagamos el pin anterior
-    // En el caso de 0 no hace falta ya que por defecto está apagado
-    // Cuidado que nos salimos del rango del array 0-1=-1 no existe este elemento
-    if (i > 0)
+    // Este primer bucle recorre el array de izquierda a derecha
+    for (int i = 0; i < 5; i++)
     {
-      // Apagamos el LED a la izquierda
-      digitalWrite(leds[i - 1], LOW);
+        // Solo para el segundo pin y consecutivos apagamos el pin anterior
+        // En el caso de 0 no hace falta ya que por defecto está apagado
+        // Cuidado que nos salimos del rango del array 0-1=-1 no existe este elemento
+        if (i > 0)
+        {
+            // Apagamos el LED a la izquierda
+            digitalWrite(leds[i - 1], LOW);
+        }
+        // Encendemos en el LED en el que estamos
+        digitalWrite(leds[i], HIGH);
+ 
+        // Esperamos el tiempo marcado por velocidad
+        delay(velocidad);
     }
- 
-    // Encendemos en el LED en el que estamos
-    digitalWrite(leds[i], HIGH);
- 
-    // Esperamos el tiempo marcado por velocidad
-    delay(velocidad);
-  }
- 
-  // Apagamos el último LED encendido, el elemento 5 del array
-  digitalWrite(leds[4], LOW);
- 
-  // Recorremos el array en sentido inverso de derecha a izquierda
-  for (int i = 4; i >= 0; i--)
-  {
-    // En el primer caso como ya está apagado el LED no hacemos nada
-    // Cuidado que nos salimos del rango del array 4+1=5 no existe este elemento
-    if (i < 4)
+    // Apagamos el último LED encendido, el elemento 5 del array
+    digitalWrite(leds[4], LOW);
+    
+    // Recorremos el array en sentido inverso de derecha a izquierda
+    for (int i = 4; i >= 0; i--)
     {
-      // Apagamos el LED a la derecha
-      digitalWrite(leds[i + 1], LOW);
-    }
-    // Encendemos en el LED en el que estamos
-    digitalWrite(leds[i], HIGH);
- 
-    // Esperamos el tiempo marcado por velocidad
-    delay(velocidad);
-  }
- 
-  // Apagamos el último LED encendido, el elemento 0 del array
-  digitalWrite(leds[0], LOW);
- 
+        // En el primer caso como ya está apagado el LED no hacemos nada
+        // Cuidado que nos salimos del rango del array 4+1=5 no existe este elemento
+        if (i < 4)
+        {
+            // Apagamos el LED a la derecha
+            digitalWrite(leds[i + 1], LOW);
+        }
+        // Encendemos en el LED en el que estamos
+        digitalWrite(leds[i], HIGH);
+        
+        // Esperamos el tiempo marcado por velocidad
+        delay(velocidad);
+      }
+      // Apagamos el último LED encendido, el elemento 0 del array
+      digitalWrite(leds[0], LOW);
 }
  
 // ISR pin 2, disminuye la velocidad
 void velocidadMenos()
 {
-  // Disminuimos el valor establecido
-  velocidad = velocidad - aumenta;
+    // Disminuimos el valor establecido
+    velocidad = velocidad - aumenta;
  
-  // Si hemos llegado a la velocidad mínima no disminuímos más
-  if (velocidad < minima)
-  {
-    velocidad = minima;
-  }
+    // Si hemos llegado a la velocidad mínima no disminuímos más
+    if (velocidad < minima)
+    {
+        velocidad = minima;
+    }
 }
  
 // ISR pin 3, aumenta la velocidad
 void velocidadMas()
 {
-  // Aumentamos el valor establecido
-  velocidad = velocidad + aumenta;
- 
-  // Si hemos llegado a la velocidad máxima no aumentamos más
-  if (velocidad > maxima)
-  {
-    velocidad = maxima;
-  }
+    // Aumentamos el valor establecido
+    velocidad = velocidad + aumenta;
+    
+    // Si hemos llegado a la velocidad máxima no aumentamos más
+    if (velocidad > maxima)
+    {    
+        velocidad = maxima;
+    }
 }
